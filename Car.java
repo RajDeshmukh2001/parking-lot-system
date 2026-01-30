ce97cfae8b8c735b2b5de5561cb01b8c0f15a907
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-30 20:15:15 +0530

    feat: add Car class extending Vehicle with custom constructor

--- Car.java
+++ Car.java
@@ -1,7 +1,8 @@
 public class Car extends Vehicle {
-    public Car(String id, String registrationNumber) {
+    public Car(String id, String registrationNumber, String slotId) {
         setId(id);
         setRegistrationNumber(registrationNumber);
-        setInTimeStamp(registrationNumber);
+        setInTimeStamp();
+        setSlotId(slotId);
     }
 }
