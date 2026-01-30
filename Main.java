a5d503ffd219e1dbb217536e7d7189ddc4aad674
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-30 21:18:02 +0530

    fix: Added message while exiting CLI

--- Main.java
+++ Main.java
@@ -29,6 +29,7 @@ public class Main {
                     return;
 
                 default:
+                    System.out.println("Exiting system!");
                     break;
             }
 
