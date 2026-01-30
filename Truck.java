76d41df57922abdb1f03223303289e9499905fe8
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-30 20:22:42 +0530

    feat: add Truck class extending Vehicle with custom constructor

---
+++ Truck.java
@@ -0,0 +1,8 @@
+public class Truck extends Vehicle {
+    public Truck(String id, String registrationNumber, String slotId) {
+        setId(id);
+        setRegistrationNumber(registrationNumber);
+        setInTimeStamp();
+        setSlotId(slotId);
+    }
+}
