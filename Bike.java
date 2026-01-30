0b11076316f554120a3a8239c948456010b22cf6
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-30 20:23:38 +0530

    feat: add Bike class extending Vehicle with custom constructor

---
+++ Bike.java
@@ -0,0 +1,8 @@
+public class Bike extends Vehicle {
+     public Bike(String id, String registrationNumber, String slotId) {
+        setId(id);
+        setRegistrationNumber(registrationNumber);
+        setInTimeStamp();
+        setSlotId(slotId);
+    }
+}
