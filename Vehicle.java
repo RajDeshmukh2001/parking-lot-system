83841c81bbc0ab5f28324d1b265ef0b80422177f
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-30 20:01:00 +0530

    feat: add setter for id in Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -5,19 +5,15 @@ public abstract class Vehicle {
     private String outTimeStamp; 
     private String slotId; 
 
-    public Vehicle(String id, String registrationNumber) { 
-        this.id = id; 
-        this.registrationNumber = registrationNumber; 
-        this.inTimeStamp = java.time.LocalDateTime.now().toString(); 
-        this.outTimeStamp = null; 
-        this.slotId = null;
-    }
-
     public void setOutTimeStamp(String outTimeStamp) {
         this.outTimeStamp = outTimeStamp;
     }
     public void setSlotId(String slotId) {
         this.slotId = slotId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
 }
