34d0501f3049a71ce122c59bdc17761041b64d36
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-01-30 19:48:38 +0530

    chore: create Slot entity class with attributes

---
+++ Slot.java
@@ -0,0 +1,11 @@
+public class Slot {
+    private int id;
+    public enum Type {
+    SMALL, MEDIUM, LARGE
+}
+    private Type type;
+    private int floorNo;
+    private boolean occupied;
+    private String vehicleId;
+
+}
