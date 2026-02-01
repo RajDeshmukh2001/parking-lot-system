f00c0c9eefb75e26a69f2ff4f97bc2dd4864dfe2
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-01 20:28:08 +0530

    feat: initialize parking lot floors and slots from user input

--- Main.java
+++ Main.java
@@ -3,8 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Parking Lot System!");
         Scanner scanner = new Scanner(System.in);
+        ParkingLot parkingLot = new ParkingLot();
+
+        int floors = Validator.readInt(scanner, "Enter number of Floors: ");
+
+        for (int i = 1; i <= floors; i++) {
+            System.out.println("Floor " + i);
+
+            int small = Validator.readInt(scanner, "Small slots: ");
+            int medium = Validator.readInt(scanner, "Medium slots: ");
+            int large = Validator.readInt(scanner, "Large slots: ");
+
+            for (int j = 0; j < small; j++) {
+                parkingLot.addSlot(SlotSize.SMALL, String.valueOf(i));
+            }
+            for (int j = 0; j < medium; j++) {
+                parkingLot.addSlot(SlotSize.MEDIUM, String.valueOf(i));
+            }
+            for (int j = 0; j < large; j++) {
+                parkingLot.addSlot(SlotSize.LARGE, String.valueOf(i));
+            }
+        }
 
         while (true) {
             System.out.println("1. Park Vehicle");
@@ -15,7 +35,7 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
-                
+
                 case 1:
                     // Park
                     break;
