251cb3a72a1cec0e142194d7be1d39d75d31d756
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-02 13:25:54 +0530

    feat:add ParkVehicleInSlot method for bike

--- ParkingLot.java
+++ ParkingLot.java
@@ -3,5 +3,10 @@ import java.util.List;
 
 public class ParkingLot {
     List<Vehicle> vehicleList=new ArrayList<>();
-    
+
+    public void parkVehicleInSlot(Bike bike, Slot slot) {
+        slot.setOccupied(true);
+        vehicleList.add(bike);
+        slot.setVehicleId(bike.getId());
+    }
 }
