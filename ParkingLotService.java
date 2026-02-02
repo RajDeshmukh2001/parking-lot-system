ac1cd9d50b8bab41f3c5792ac95fd0e12bd4f755
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-02 13:30:04 +0530

    feat(parking):add parkVehicle method in ParkingLotService.java

--- ParkingLotService.java
+++ ParkingLotService.java
@@ -32,4 +32,34 @@ public class ParkingLotService {
                 return false;
         }
     }
+
+    public static void parkVehicle(String vehicleType, String vehicleRegistrationNumber) {
+        //first the first available slot for the vehicle type is found
+        //the first floor slots will be checked first and filled and so does for other floors
+        List<Slot> slots=parkingLot.getAllParkingSlots();
+        //the slots is alread sorted by floor number while creating parking lot
+        
+        for(Slot slot:slots) {
+            if(!slot.isOccupied()) {
+                if(vehicleType.equals("bike") && slot.getSize()==SlotSize.SMALL) {
+                    Bike bike=new Bike( vehicleRegistrationNumber, slot.getId());
+                    parkingLot.parkVehicleInSlot(bike, slot);
+                    System.out.println("Bike parked in slot: "+slot.getId());
+                    return;
+                }
+                else if(vehicleType.equals("car") && (slot.getSize()==SlotSize.MEDIUM || slot.getSize()==SlotSize.LARGE)) {
+                    Car car=new Car( vehicleRegistrationNumber, slot.getId());
+                    parkingLot.parkVehicleInSlot(car, slot);
+                    System.out.println("Car parked in slot: "+slot.getId());
+                    return;
+                }
+                else if(vehicleType.equals("truck") && slot.getSize()==SlotSize.LARGE) {
+                    Truck truck=new Truck( vehicleRegistrationNumber, slot.getId());
+                    parkingLot.parkVehicleInSlot(truck, slot);
+                    System.out.println("Truck parked in slot: "+slot.getId());
+                    return;
+                }
+            }
+        }
+    }
 }
