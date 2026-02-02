36e26cc9029bcc79d97f3f8485e8aaa9e1ecb5df
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-02 12:25:34 +0530

    feat(parkig):add vehicle registration number validation method in InputValidator.java

--- InputValidator.java
+++ InputValidator.java
@@ -1,8 +1,9 @@
 public class InputValidator {
     
-    public static boolean isValidVehicleNumber(String vehicleNumber) {
+    public static boolean isValidVehicleRegistrationNumber(String vehicleNumber) {
         //for indian vehicle number validation
-        String vehicleNumberPattern = "^[A-Z]{2}\\s?[0-9]{1,2}\\s?[A-Z]{1,2}\\s?[0-9]{1,2,3,4}$";
+        vehicleNumber=vehicleNumber.trim().toUpperCase();
+        String vehicleNumberPattern = "^[A-Z]{2}\\s?[0-9]{1,2}\\s?[A-Z]{1,2}\\s?[0-9]{3,4}$";
         return vehicleNumber.matches(vehicleNumberPattern);
     }
 }
