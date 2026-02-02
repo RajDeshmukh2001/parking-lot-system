eb1d0fe805f234b639792f85579af1bc36f2862c
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-02 11:20:07 +0530

    refactor(parking): rename the method name getSlots to getAllParkingSlots

--- ParkingLotService.java
+++ ParkingLotService.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class ParkingLotService {
     private static  ParkingLot parkingLot = new ParkingLot();
     public static boolean isSlotAvailableBySlotSize(SlotSize slotSize) {
-        List<Slot> slots=parkingLot.getSlots();
+        List<Slot> slots=parkingLot.getAllParkingSlots();
         for(Slot slot:slots) {
             if(!slot.isOccupied() && slot.getSize()==slotSize) {
                 return true;
