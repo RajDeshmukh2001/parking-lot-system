d24effac947ce844b4b1979ae415a1c546f2aa03
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-02 10:48:12 +0530

    feat: Create the slot creation based on user input

--- ParkingLot.java
+++ ParkingLot.java
@@ -5,11 +5,6 @@ public class ParkingLot {
     private List<Slot> slotList = new ArrayList<>();
 
     public void addSlot(SlotSize size, String floorNumber) {
-        Slot slot = new Slot();
-        slot.setId(UUID.randomUUID().toString());
-        slot.setSize(size);
-        slot.setFloorNumber(floorNumber);
-        slot.setOccupied(false);
-        slotList.add(slot);
+        slotList.add(new Slot(UUID.randomUUID().toString(), size, floorNumber));
     }
 }
